848b1d88089543e7add8f29ac3e779e3272bc3d3
somin48 <somin2681@gmail.com>
2024-12-21 16:00:38 -0500

    CORS update

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectToCL();
 app.use(express.json());
 
 // Configure CORS
-const allowedOrigins = ['http://localhost:5173', 'https://4413pj-front.vercel.app', 'https://4413pj-admin.vercel.app']; // Replace with your actual frontend URLs
+const allowedOrigins = ['http://localhost:5173', 'https://4413pj-front.vercel.app', 'https://4413pj-admin.vercel.app', 'https://4413pj-admin-erwzwa52i-ss-projects-88cfae83.vercel.app']; // Replace with your actual frontend URLs
 app.use(cors({
     origin: allowedOrigins,
     credentials: true, // If using cookies or HTTP authentication
